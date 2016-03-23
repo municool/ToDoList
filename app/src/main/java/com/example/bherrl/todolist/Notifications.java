9d603287be7122fca003375f00b0f35f62984d10
Osaia <jessicadominguez95@gmail.com>
2016-03-23 16:02:43 +0100

    Notification pdate

--- app/src/main/java/com/example/bherrl/todolist/Notifications.java
+++ app/src/main/java/com/example/bherrl/todolist/Notifications.java
@@ -32,12 +32,6 @@ public class Notifications extends BroadcastReceiver{
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
 
-//        Notification notification = builder.setContentTitle("Demo App Notification")
-//                .setContentText("New Notification From Demo App..")
-//                .setTicker("New Message Alert!")
-//                .setSmallIcon(R.mipmap.ic_launcher)
-//                .setContentIntent(pendingIntent).build();
-
         Notification notification = builder
                         .setContentTitle("Oh my Gosh ...")
                         .setSmallIcon(R.mipmap.ic_tick)
@@ -47,6 +41,7 @@ public class Notifications extends BroadcastReceiver{
         //Bit to be bitwise-ored into the flags field that should be set if the notification should be canceled when it is clicked by the user
         //The Line below makes the Notification disappear when clicked
         notification.flags = Notification.DEFAULT_LIGHTS | Notification.FLAG_AUTO_CANCEL;
+        notification.flags = Notification.FLAG_SHOW_LIGHTS;
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(0, notification);
